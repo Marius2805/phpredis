ef8f40bd3b2c1581f1b9bc99487de8510d5175e2
Marius Meißner <marius.meissner@sixt.com>
2017-03-28 14:32:51 +0200

    PHPREDIS-37: Removed output from startSession.php

--- tests/startSession.php
+++ tests/startSession.php
@@ -3,15 +3,9 @@ $sessionId = $argv[1];
 $sleepTime = $argv[2];
 $maxExecutionTime = $argv[3];
 
-$id = uniqid();
-
 ini_set('max_execution_time', $maxExecutionTime);
 
 session_id($sessionId);
-
 session_start();
-echo "$id: Started session \n";
-
-echo "$id: Sleeping \n";
 sleep($sleepTime);
 
